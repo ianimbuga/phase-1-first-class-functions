1fbbe5f0cf767bb3d91e0f96b6a410e3eead718a
ianimbuga <imbugaian20@gmail.com>
2024-09-24 22:31:35 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,22 @@
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("I am a named function");
+    }
+    return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("I am an anonymous functional");
+    };
+}
+
+module.exports = {
+recievesAFunction,
+returnsANamedFunction,
+returnsAnAnonymousFunction,
+
+};
